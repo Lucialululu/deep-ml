30460d9810143fda2ada086c0e70127a024a25e7
Lucialululu <118808834+Lucialululu@users.noreply.github.com>
2026-09-21 14:26:37 +0000

    Solve: Linear Regression Using Normal Equation

---
+++ problems/0014-linear-regression-using-normal-equation/solution.py
@@ -0,0 +1,7 @@
+import numpy as np
+def linear_regression_normal_equation(X: list[list[float]], y: list[float]) -> list[float]:
+	# Your code here, make sure to round
+	X_arr = np.array(X)
+	y_arr = np.array(y)
+	theta = np.linalg.inv(X_arr.T @ X_arr) @ (X_arr.T @ y_arr)
+	return theta
